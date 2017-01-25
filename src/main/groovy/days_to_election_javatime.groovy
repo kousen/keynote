1a8a58b8a28a7d1ca32ce1fdb3a481b5b35acab2
Kenneth Kousen <ken.kousen@kousenit.com>
2017-01-25 12:53:33 -0500

    Add pluralization to date output

--- src/main/groovy/days_to_election_javatime.groovy
+++ src/main/groovy/days_to_election_javatime.groovy
@@ -9,9 +9,16 @@ LocalDate now = LocalDate.now()
 
 println "${DAYS.between(now, electionDay)} days to go..."
 
+String pluralize(int num) {
+    num == 1 ? '' : 's'
+}
+
+assert pluralize(1) == ''
+(2..31).each { assert pluralize(it) == 's' }
+
 int years  = YEARS.between(now, electionDay)
 int months = MONTHS.between(now.plusYears(years), electionDay)
 int days   = DAYS.between(now.plusYears(years).plusMonths(months), electionDay)
-println "($years years, $months months, and $days day${days == 1 ? '' : 's'})"
+println "($years year${pluralize(years)}, $months month${pluralize(months)}, and $days day${pluralize(days)})"
 
 assert now.plusYears(years).plusMonths(months).plusDays(days) == electionDay
